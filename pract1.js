55d58c65c6130c4201c1cb58ef6dbb11442b6a60
raulperez7778543-cloud <raulperez.7778543@gmail.com>
2026-09-01 12:37:31 -0600

    Add files via upload

---
+++ pract1.js
@@ -0,0 +1,13 @@
+var nombre = "raul"
+var edad = 22;
+var apeido= "becerra";
+var presente= true;
+var estatura=1.72;
+console.log("Mi nombre es: "+nombre);
+console.log("Mi apellido es: "+apeido);
+console.log("presente: " +presente);
+console.log("Mi edad es: "+edad);
+console.log("Mi estatura es: "+estatura);
+
+
+//alert ("mi nombre es"+nombre)
